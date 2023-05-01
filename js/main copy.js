37fe3cf8f2f8f9ac11a8806d3628af99af468a80
skykwon1004 <118698700+skykwon1004@users.noreply.github.com>
2023-05-01 14:54:45 +0900

    Add files via upload

--- js/main copy.js
+++ js/main copy.js
@@ -1,31 +1,17 @@
 $(function () {
 
     $('.mainFull').fullpage({
-        anchors: ['intro', 'l02', 'l03', 'l04', 'l05', 'l06'],
+        anchors: ['intro', 'l02', 'l03', 'l04', 'footer'],
         afterLoad: function (lnk, idx) {
             // console.log(lnk, idx);
             // 만약에 2번째에 오면 ... nav li class on을 붙이고 나머지 a 는 뗀다.
             // $('nav li a').removeClass('on');
 
-            if (idx == 2 || idx == 4 || idx == 6) {
-                // $('nav li').eq(1).find('a').addClass('on');
-                $('.header').addClass('on');
-            } else {
-                $('.header').removeClass('on');
-            }
-
-            if (idx == 2 || idx == 4 || idx == 6) {
+            if (idx == 2 || idx == 4) {
                 // $('nav li').eq(1).find('a').addClass('on');
                 $('nav').addClass('on');
             } else {
                 $('nav').removeClass('on');
-            }
-
-            // 페이지 넘어갈 때 마다 logo 이미지 바꾸기
-            if (idx == 2 || idx == 4 || idx == 6) {
-                $(".logo").attr("src", "./img/logo_dark.png");
-            } else {
-                $(".logo").attr("src", "./img/logo_white.png");
             }
 
             if (idx == 1) {
@@ -47,10 +33,15 @@ $(function () {
             $('nav li').eq(nIdx - 1).addClass('on').siblings().removeClass('on')
         },
 
+
+
+
         // navigation: true,
         // navigationPosition: 'left',
         // navigationTooltips: ['firstSlide', 'secondSlide'],
     });
+
+
 
     $('.main_slide').slick({
         arrows: false,
@@ -59,6 +50,9 @@ $(function () {
         autoplay: true,
         autoplaySpeed: 2000,
     });
+
+
+
 
 
 })
